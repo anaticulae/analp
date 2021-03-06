07d0075cd1461af184092a83424e7d39d512f0a0
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-03-06 19:31:05 +0100

    0.2.0 auto generated release
    Feature
    
    * add wrapper to tokenize sentences and words (84511c9ec190)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 Every noteable change is logged here.
 
+## v0.2.0
+
+### Feature
+
+* add wrapper to tokenize sentences and words (84511c9ec190)
+
 ## v0.1.0
 
 ### Feature
--- knlp/__init__.py
+++ knlp/__init__.py
@@ -13,6 +13,6 @@ from knlp.sentence import normalize as normalize_sentence
 from knlp.sentence import sent_tokenize
 from knlp.word import word_tokenize
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
