22c37d79a2fee7484376b0e857c9b189c2f5d4f2
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-06-12 21:10:12 +0200

    feat(word): escape german sch-pattern

--- knlp/word.py
+++ knlp/word.py
@@ -14,6 +14,34 @@ def word_tokenize(sentence: str, language='german') -> list:
     """\
     >>> word_tokenize('Ein Aufzählung z.B. heute oder morgen 1. Frosch.')
     ['Ein', 'Aufzählung', 'z.B.', 'heute', 'oder', 'morgen', '1', '.', 'Frosch', '.']
+    >>> word_tokenize('Hier lässt sich auch der Luhmann’sche Personenbegriff angliedern.', language='science')
+    ['Hier', 'lässt', 'sich', 'auch', 'der', 'Luhmann’sche', 'Personenbegriff', 'angliedern', '.']
     """
+    sentence = escape(sentence)
     tokenized = nltk.tokenize.word_tokenize(sentence, language=language)
-    return list(tokenized)
+    result = deescape(tokenized)
+    return result
+
+
+ESCAPE = """\
+’sche           QUOTATION_SCHE              # Luhmann’sche
+’SCHE           QUOTATION_SCHE_UPPER
+'sche           QUOTATION_SCHE_SINGLE
+'SCHE           QUOTATION_SCHE_UPPER_SINGLE
+"""
+ESCAPE: dict = {
+    line.split()[0]: line.split()[1] for line in ESCAPE.strip().splitlines()
+}
+
+
+def escape(text: str):
+    for key, value in ESCAPE.items():
+        text = text.replace(key, value)
+    return text
+
+
+def deescape(text: list):
+    # escape mann_sche
+    for key, value in ESCAPE.items():
+        text = [item.replace(value, key) for item in text]
+    return text
