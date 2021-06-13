5d61d8657f7c9d187fa1634d1d2c8cd604ac0b0f
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-06-13 19:53:29 +0200

    feat(word): escape special signs

---
+++ knlp/utils.py
@@ -0,0 +1,59 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2021 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import unicodedata
+
+
+def hashed(item: str) -> str:
+    """\
+    >>> hashed('‘SCHEN')
+    'LEFTSINGLEQUOTATIONMARKSCHEN'
+    """
+    selected = [
+        unicodedata.name(char) if ord(char) > 256 else char for char in item
+    ]
+    selected = ''.join(selected).replace(' ', '')
+    return selected
+
+
+ESCAPE = """\
+‘schen
+‘SCHEN
+’schen
+’SCHEN
+'schen
+'SCHEN
+‘sche
+’sche
+’SCHE
+'sche
+'SCHE
+‘s
+‘S
+’s
+’S
+'s
+'S
+"""
+ESCAPE: dict = {
+    f'{line} ': f'{hashed(line)} ' for line in ESCAPE.strip().splitlines()
+}
+
+
+def escape(text: str):
+    for key, value in ESCAPE.items():
+        text = text.replace(key, value)
+    return text
+
+
+def deescape(text: list):
+    # escape mann_sche
+    for key, value in ESCAPE.items():
+        text = [item.replace(value.strip(), key.strip()) for item in text]
+    return text
--- knlp/word.py
+++ knlp/word.py
@@ -9,6 +9,8 @@
 
 import nltk.tokenize
 
+import knlp.utils
+
 
 def word_tokenize(sentence: str, language='german') -> list:
     """\
@@ -16,32 +18,10 @@ def word_tokenize(sentence: str, language='german') -> list:
     ['Ein', 'Aufzählung', 'z.B.', 'heute', 'oder', 'morgen', '1', '.', 'Frosch', '.']
     >>> word_tokenize('Hier lässt sich auch der Luhmann’sche Personenbegriff angliedern.', language='science')
     ['Hier', 'lässt', 'sich', 'auch', 'der', 'Luhmann’sche', 'Personenbegriff', 'angliedern', '.']
+    >>> word_tokenize('Clay Shirky‘s Writings About the Internet.', language='science')
+    ['Clay', 'Shirky‘s', 'Writings', 'About', 'the', 'Internet', '.']
     """
-    sentence = escape(sentence)
+    sentence = knlp.utils.escape(sentence)
     tokenized = nltk.tokenize.word_tokenize(sentence, language=language)
-    result = deescape(tokenized)
+    result = knlp.utils.deescape(tokenized)
     return result
-
-
-ESCAPE = """\
-’sche           QUOTATION_SCHE              # Luhmann’sche, Luhmann’schen
-’SCHE           QUOTATION_SCHE_UPPER
-'sche           QUOTATION_SCHE_SINGLE
-'SCHE           QUOTATION_SCHE_UPPER_SINGLE
-"""
-ESCAPE: dict = {
-    line.split()[0]: line.split()[1] for line in ESCAPE.strip().splitlines()
-}
-
-
-def escape(text: str):
-    for key, value in ESCAPE.items():
-        text = text.replace(key, value)
-    return text
-
-
-def deescape(text: list):
-    # escape mann_sche
-    for key, value in ESCAPE.items():
-        text = [item.replace(value, key) for item in text]
-    return text
